0fe1a92f0192219b8c840893b4066244eb4724c2
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2015-12-05 09:20:25 +0900

    predict the next hand

--- usr/s13t208_02/PokerOpe.c
+++ usr/s13t208_02/PokerOpe.c
@@ -61,13 +61,110 @@ us : 捨札数
 
 --------------------------------------------------------------------*/
 
+#define CARDNUM 52 //カード総数
+
+/*--------------------------------------------------------------------*/
+
+void outputCard( int deck[], int num ) {
+  int i;
+
+  for ( i = 0; i < 13; i++) {
+    printf("%2d ", i+1);
+  }
+  printf("\n");
+  for ( i = 1; i <= num; i++) {
+    printf("%2d ", deck[i-1]);
+    if ( i%13 == 0 && i > 0 ) { printf("\n"); }
+  }
+  printf("\n\n");
+}
+ 
+// 山札と既に場に出た札を合わせたものを生成
+// 山札 +1, それ以外 -1
+void makeDeck( int hd[], int fd[], int cg, int tk, int ud[], int us, int deck[] ) {
+  int i;
+  for ( i = 0; i < 5; i++) {
+    deck[hd[i]] = -1;
+  }
+
+  for ( i = 0; i < cg; i++) {
+    deck[fd[i]] = -1;
+  }
+  for ( i = 0; i < us; i++) {
+    deck[ud[i]] = -1;
+  }
+  
+  for ( i = 0; i < CARDNUM; i++) {
+    if ( deck[i] == 0 ) { deck[i] = 1; }
+  }
+}
+
+// デッキの残り札 cg, usとか使えばいらないかも
+int deckNum( int deck[] ) {
+  int i;
+  int num = 0;
+
+  for ( i = 0; i < CARDNUM; i++) {
+    if ( deck[i] == 1 ) { num++; }
+  }
+  return num;
+}
+
+void copyHd( int hd[], int nextHd[]) {
+  int i;
+  for ( i = 0; i < 5; i++) {
+    nextHd[i] = hd[i];
+  }
+}
+
+double calcExp( int hd[], int fd[], int cg, int tk, int ud[], int us, int deck[], int changeCard ) {
+  int i;
+  int nextHd[5]; //次の手札
+  double exp = 0;
+
+  for ( i = 0; i < CARDNUM; i++) {
+    if ( deck[i] == 1 ) {
+      copyHd(hd, nextHd);
+      nextHd[changeCard] = i;
+      exp += poker_point(nextHd)/(double)deckNum(deck);
+    }
+  }
+  return exp;
+}
+
+// 捨てる手札でもっとも期待値が高くなる札番号を返す
+int selectCard( int hd[], int fd[], int cg, int tk, int ud[], int us, int deck[] ) {
+  int i;
+  double hightestExp = poker_point(hd); // 現在の手札の点数
+  double exp;
+  int select = -1; //捨てる手札の番号
+
+  for ( i = 0; i < 5; i++ ) {
+    exp = calcExp( hd, fd, cg, tk , ud, us, deck, i );
+    // 現在より期待値が高ければselectを変更
+    if ( exp > hightestExp ) { 
+      select = i; 
+      hightestExp = exp;
+    }
+  }
+  return select;
+}
+
+// 捨てると最も期待値が高くなるカードを返す
+int bestExp( int hd[], int fd[], int cg, int tk, int ud[], int us ) {
+  int deck[CARDNUM] = { 0 };
+
+  makeDeck(hd, fd, cg, tk , ud, us, deck );
+
+  return selectCard( hd, fd, cg, tk , ud, us, deck );
+}
+
 int strategy( int hd[], int fd[], int cg, int tk, int ud[], int us) {
   int myhd[HNUM];
   int select = -1;
   int point;
 
   int i;
-
 
   //高い役の時終了
   point = poker_point( hd ); //手札の点数
@@ -78,7 +175,9 @@ int strategy( int hd[], int fd[], int cg, int tk, int ud[], int us) {
   if ( point == P6 ) { return -1; } //フルハウス
   if ( point == P4 ) { return -1; } //ストレート
 
-  return 1;
+  select = bestExp( hd, fd, cg, tk, ud, us );
+
+  return select;
 }
 
 //====================================================================
