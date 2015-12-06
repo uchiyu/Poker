5a35423cfa559e54cc7277ea5b4a4353471e60a1
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2015-12-06 15:36:47 +0900

    add optimisation compile setting

--- sys/BinCompile.sh
+++ sys/BinCompile.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-/usr/bin/gcc -I${1} -DCHNG=${2} -DTAKE=${3} -o PokerExec PokerOpe.c ../../src/CardLib.c ../../src/PokerExec.c
+/usr/bin/gcc -O3 -I${1} -DCHNG=${2} -DTAKE=${3} -o PokerExec PokerOpe.c ../../src/CardLib.c ../../src/PokerExec.c
